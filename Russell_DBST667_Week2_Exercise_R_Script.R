e2060dac03e0b6be9c385329e21e589c2572d329
Brandon Russell <rbrandon87@gmail.com>
2018-05-29 17:24:57 -0400

    Update Two
    Update from laptop, working on exercise

--- Russell_DBST667_Week2_Exercise_R_Script.R
+++ Russell_DBST667_Week2_Exercise_R_Script.R
@@ -1,3 +1,11 @@
 #Part 2aii
 credit <- utils::read.csv("CreditApproval.csv")
 head(credit, n=10)
+#Part 2aiii
+str(credit)
+#Part 2bi
+summary(credit)
+#Part2bii
+summary(credit$A14)
+summary(credit$A2)
+#Part2biii
